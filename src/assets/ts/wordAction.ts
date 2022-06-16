d4a9660f60220ac388f38915cbaac0361d03e0b6
Gyunam Han <thslrrbska@gmail.com>
2022-06-16 19:02:04 +0900

    wordAction confirmWord upgrade validator

--- src/assets/ts/wordAction.ts
+++ src/assets/ts/wordAction.ts
@@ -22,17 +22,22 @@ export default class WordAction implements Actions {
     this.word = this.word.slice(0, -1);
   }
   async confirmWord(): Promise<boolean> {
-    this.isFindingDictionary = true;
-    try {
-      const res = await findDictionary(this.word);
-      if (res.data.length) return true;
-      alert("단어를 찾을 수 없습니다.");
+    if (this.word.length === this.limited) {
+      this.isFindingDictionary = true;
+      try {
+        const res = await findDictionary(this.word);
+        if (res.data.length) return true;
+        alert("단어를 찾을 수 없습니다.");
+        return false;
+      } catch (error) {
+        alert("단어를 찾을 수 없습니다.");
+        return false;
+      } finally {
+        this.isFindingDictionary = false;
+      }
+    } else {
+      alert("단어를 전부 입력해주세요.");
       return false;
-    } catch (error) {
-      alert("단어를 찾을 수 없습니다.");
-      return false;
-    } finally {
-      this.isFindingDictionary = false;
     }
   }
 
